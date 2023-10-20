04afe019113cf7f9a98ece043a25cc82e4910e03
Julia Evans <julia@jvns.ca>
2023-10-20 16:29:28 -0400

    update curses example

--- curses_example.py
+++ curses_example.py
@@ -2,60 +2,83 @@ import curses
 import time
 
 
-def main(stdscr):
-    # Setup the curses environment
-    curses.curs_set(0)  # Hide the cursor
-    stdscr.nodelay(1)  # Do not wait for user input when refreshing the screen
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)  # Setup a color pair
+class CursesApp:
+    def __init__(self):
+        self.items = ["Item 1", "Item 2", "Item 3", "Item 4"]
+        self.current_item = 0
+        self.stdscr = None
+        self.left_win = None
+        self.right_win = None
 
-    # Calculate window dimensions
-    max_y, max_x = stdscr.getmaxyx()
-    center_x = max_x // 2
-    left_win = stdscr.subwin(max_y, center_x, 0, 0)  # Create the left window
-    right_win = stdscr.subwin(
-        max_y, max_x - center_x, 0, center_x
-    )  # Create the right window
+    def run(self, stdscr):
+        self.stdscr = stdscr
+        self.setup_curses()
+        self.main_loop()
 
-    items = ["Item 1", "Item 2", "Item 3", "Item 4"]  # Example items
-    current_item = 0  # Currently selected item
+    def setup_curses(self):
+        curses.curs_set(0)
+        self.stdscr.nodelay(1)
+        curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_WHITE)
+        self.update_windows()
 
-    while True:
-        # Clear the windows to prepare for new content
-        left_win.clear()
-        left_win.box()
-        right_win.clear()
-        right_win.box()
+    def main_loop(self):
+        while True:
+            self.draw_items()
+            self.draw_details()
 
-        # Draw the items on the left window
-        for index, item in enumerate(items):
-            if index == current_item:
-                left_win.addstr(
-                    index, 1, item, curses.color_pair(1)
-                )  # Highlight the selected item
+            self.left_win.refresh()
+            self.right_win.refresh()
+
+            self.handle_input()
+
+    def update_windows(self):
+        max_y, max_x = self.stdscr.getmaxyx()
+        center_x = max_x // 2
+
+        if self.left_win:
+            del self.left_win  # Delete the previous window object to avoid memory leak
+        self.left_win = self.stdscr.subwin(max_y, center_x, 0, 0)
+        self.left_win.box()
+
+        if self.right_win:
+            del self.right_win
+        self.right_win = self.stdscr.subwin(max_y, max_x - center_x, 0, center_x)
+        self.right_win.box()
+
+    def draw_items(self):
+        self.left_win.clear()
+        self.left_win.box()  # Re-draw box after clearing
+        for index, item in enumerate(self.items):
+            if index == self.current_item:
+                self.left_win.addstr(
+                    index + 1, 1, item, curses.color_pair(1)
+                )  # +1 to account for box's border
             else:
-                left_win.addstr(index, 1, item)
+                self.left_win.addstr(index + 1, 1, item)
 
-        # Update the right window based on the selected item
-        right_win.addstr(0, 1, f"Details for {items[current_item]}")
+    def draw_details(self):
+        self.right_win.clear()
+        self.right_win.box()  # Re-draw box after clearing
+        self.right_win.addstr(
+            1, 1, f"Details for {self.items[self.current_item]}"
+        )  # +1 to account for box's border
 
-        # Refresh the windows to display changes
-        left_win.refresh()
-        right_win.refresh()
+    def handle_input(self):
+        key = self.stdscr.getch()
 
-        time.sleep(0.1)  # Sleep for a bit to avoid 100% CPU usage
-        # Capture user's key press
-        key = stdscr.getch()
-
-        # If the user presses 'q', quit the program
-        if key == ord("q"):
-            break
-
-        # Allow the user to navigate through the items
-        if key == curses.KEY_UP and current_item > 0:
-            current_item -= 1
-        elif key == curses.KEY_DOWN and current_item < len(items) - 1:
-            current_item += 1
+        if key == -1:
+            # No input
+            time.sleep(0.01)  # Sleep briefly to prevent 100% CPU usage
+        elif key == ord("q"):
+            exit()
+        elif key == curses.KEY_UP and self.current_item > 0:
+            self.current_item -= 1
+        elif key == curses.KEY_DOWN and self.current_item < len(self.items) - 1:
+            self.current_item += 1
+        elif key == curses.KEY_RESIZE:
+            self.update_windows()  # Recalculate window dimensions and redraw
 
 
-# Run the curses application
-curses.wrapper(main)
+if __name__ == "__main__":
+    app = CursesApp()
+    curses.wrapper(app.run)
